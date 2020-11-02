57393f11ebb9a7ece60c550390a37c2f35cc584b
Carlos Diaz <diazpolanco13@gmail.com>
2020-11-02 00:03:14 -0400

    SIRVIENDO INDEX Y CONF VARIABLES DE ENTORNO

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express')
+require('dotenv').config();
+
 
 //crear el serivor de express
 const app = express();
 
+//Directorio publico
+app.use(express.static('public'));
+
 
 //Rutas
-app.get('/', (req, res) => {
-    console.log('Se requiere el /');
-    res.json({
-        ok: true
-    })
-})
+// app.get('/', (req, res) => {
+//     console.log('Se requiere el /');
+//     res.json({
+//         ok: true
+//     })
+// })
 
 
 // Escuchar peticiones
-app.listen(4000, () => {
-    console.log(`Servidor corriendo en puerto ${ 4000 } Ok!`)
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT } Ok!`)
 });
